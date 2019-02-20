0122fcc32ef615335c5a34cfad43870e4b9fb625
Eliott <Eliotts@protonmail.com>
2019-02-20 11:31:14 +0100

    Bullet colision with wall + rebound

--- bullet.lua
+++ bullet.lua
@@ -7,7 +7,8 @@ function create_bullet(player)
     update              = update_bullet,
     draw                = draw_bullet,
     regs                = {"to_update", "to_draw2"},
-    speed               = 14, -- per second
+    speed               = 18, -- per second
+    speed_lost_rebound  = 1/2, -- per second
     v                   = { x = 0, y = 0}, -- movement vector 
     time_despawn        = 0.5, -- seconds a bullet would have at spawn before despawn
     timer_despawn       = 0, -- seconds remaining before despawn
@@ -17,17 +18,17 @@ function create_bullet(player)
   
   -- get vector
   local angle = player.angle - .015 + rnd(.03)
-  s.v.x = cos(angle) * s.speed
-  s.v.y = sin(angle) * s.speed
+  s.v.x = cos(angle)
+  s.v.y = sin(angle)
   
   --spawn according to vector
   
-  s.x = player.x + player.w / 2 * (s.v.x / s.speed) 
-  s.y = player.y + player.h / 2 * (s.v.y / s.speed)
+  s.x = player.x + player.w / 2 * (s.v.x ) 
+  s.y = player.y + player.h / 2 * (s.v.y )
   
   -- , s.y = x/2 , y/2
-  s.w = 8
-  s.h = 8
+  s.w = 6
+  s.h = 6
   
   s.timer_despawn = s.time_despawn
   register_object(s)
@@ -41,8 +42,9 @@ function update_bullet(s)
   
   if( s.timer_despawn < 0 ) then kill_bullet(s)
   elseif( s.timer_despawn > 0.1  and  s.timer_despawn <  s.time_despawn - 0.05 ) then
-    s.x = s.x + s.v.x * s.speed * delta_time
-    s.y = s.y + s.v.y * s.speed * delta_time
+    -- s.x = s.x + s.v.x * s.speed * delta_time
+    -- s.y = s.y + s.v.y * s.speed * delta_time
+    update_move_bullet(s)
   end
   --Collisions
   local player = collide_objgroup(s,"player")
@@ -58,6 +60,30 @@ function update_bullet(s)
     destroyable.alive = false
   end
       
+end
+
+function update_move_bullet(s)
+  local nx = s.x + s.v.x * s.speed * delta_time * 10
+  local col = check_mapcol(s,nx)
+  if col then
+    local tx = flr((nx + col.dir_x * s.w * 0.5) / 8)
+    s.x = tx * 8 + 4 - col.dir_x * (8 + s.w + 0.5) * 0.5
+    s.v.x = s.v.x *-1
+    s.speed = s.speed * s.speed_lost_rebound
+  else
+    s.x = nx
+  end
+  
+  local ny = s.y + s.v.y * s.speed * delta_time * 10
+  local col = check_mapcol(s,s.x,ny)
+  if col then
+    local ty = flr((ny + col.dir_y * s.h * 0.5) / 8)
+    s.y = ty * 8 + 4 - col.dir_y * (8 + s.h + 0.5) * 0.5
+    s.v.y = s.v.y *-1
+    s.speed = s.speed * s.speed_lost_rebound
+  else
+    s.y = ny
+  end
 end
 
 function draw_bullet(s)
