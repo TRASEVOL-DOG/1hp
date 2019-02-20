52e75dd447664a85edf734e403853f2ef64d7f8a
Eliott <Eliotts@protonmail.com>
2019-02-20 16:45:01 +0100

    bullet states and better cactus colision

--- bullet.lua
+++ bullet.lua
@@ -7,12 +7,12 @@ function create_bullet(player)
     -- from                = 0, -- player id
     animt               = 0,
     anim_state          = "stopped",
-    kill_anim_t         = 0,
+    kill_anim_t         = .1,
     update              = update_bullet,
     draw                = draw_bullet,
     regs                = {"to_update", "to_draw1", "bullet"},
     speed               = 18, -- per second
-    speed_lost_rebound  = 3/4,
+    speed_lost_rebound  = 1/4,
     v                   = { x = 0, y = 0}, -- movement vector
     time_despawn        = 0.8, -- seconds a bullet would have at spawn before despawn
     timer_despawn       = 0 -- seconds remaining before despawn
@@ -55,14 +55,23 @@ end
 function update_bullet(s)
   s.timer_despawn = s.timer_despawn - delta_time
   
-  if( s.anim_state ~= "killed") and ( s.timer_despawn >  s.time_despawn - 0.05 ) then 
-    anim_state = "stopped"   
-  elseif( s.timer_despawn < 0 ) then 
+  
+  if( s.timer_despawn < 0 and s.anim_state ~= "killed") then 
     kill_bullet(s)
+  elseif( s.timer_despawn >  s.time_despawn - 0.05 ) then 
+    s.anim_state = "stopped" 
+  elseif s.anim_state == "killed" then 
+    s.kill_anim_t = s.kill_anim_t - delta_time
+    if s.kill_anim_t < 0 then
+      deregister_bullet(s)
+    end
   else
-    anim_state = "moving"
+    s.anim_state = "moving"
     update_move_bullet(s)
   end
+  debuggg = s.anim_state
+  
+  
   -- Collisions
   -- local player = collide_objgroup(s,"player")
   -- if(player) then
@@ -73,11 +82,13 @@ function update_bullet(s)
   -- end
   
   local destr = all_collide_objgroup(s,"destroyable")
-  if(destr) then
+  if(#destr>0) then
     for i=1, #destr do
-      kill_destroyable(destr[i])
+      if destr[i].alive then
+        kill_destroyable(destr[i])
+        kill_bullet(s)
+      end
     end
-    kill_bullet(s)
   end
       
 end
@@ -89,7 +100,7 @@ function update_move_bullet(s)
     local tx = flr((nx + col.dir_x * s.w * 0.5) / 8)
     s.x = tx * 8 + 4 - col.dir_x * (8 + s.w + 0.5) * 0.5
     s.v.x = s.v.x *-1
-    s.speed = s.speed * s.speed_lost_rebound
+    s.speed = s.speed * ( 1 - s.speed_lost_rebound )
   else
     s.x = nx
   end
@@ -100,7 +111,7 @@ function update_move_bullet(s)
     local ty = flr((ny + col.dir_y * s.h * 0.5) / 8)
     s.y = ty * 8 + 4 - col.dir_y * (8 + s.h + 0.5) * 0.5
     s.v.y = s.v.y *-1
-    s.speed = s.speed * s.speed_lost_rebound
+    s.speed = s.speed * ( 1 - s.speed_lost_rebound )
   else
     s.y = ny
   end
@@ -109,7 +120,7 @@ end
 function draw_bullet(s)
   if s.anim_state == "stopped" then
     spr(56, s.x, s.y-2, 1, 1, atan2(s.v.x, s.v.y))
-  elseif anim_state == "killed" then 
+  elseif s.anim_state == "killed" then 
     spr(59, s.x, s.y-2, 1, 1, atan2(s.v.x, s.v.y))
   else
     spr(57, s.x, s.y-2, 2, 1, atan2(s.v.x, s.v.y))
@@ -118,8 +129,12 @@ end
 
 function kill_bullet(s)
   s.anim_state = "killed"
-  s.kill_anim = .2
+  -- s.kill_anim_t = .2
 end
+-- function kill_bullet2(s)
+  -- s.anim_state = "killed"
+  -- s.kill_anim_t = .2
+-- end
 
 function deregister_bullet(s)
   deregister_object(s)
