a9c6b68fc5f359131ccd147200ea3a1bb086fe04
Remy <trasevol.dog@gmail.com>
2019-02-21 23:50:45 +0100

    Desync fixing attempt

--- player.lua
+++ player.lua
@@ -167,10 +167,10 @@ function update_player(s)
       local dd = mid(1 - (s.speed / s.max_speed) - abs(s.dx_input) - abs(s.dy_input), 0, 1)
       
       if abs(dx) >= 1 then
-        s.v.x = s.v.x - (sgn(dx) * 2 + dd * dx/2) * s.acceleration * delta_time * 5
+        s.v.x = s.v.x - (sgn(dx) * 0.75 + dd * dx/2) * s.acceleration * delta_time * 5
       end
       if abs(dy) >= 1 then
-        s.v.y = s.v.y - (sgn(dy) * 2 + dd * dy/2) * s.acceleration * delta_time * 5
+        s.v.y = s.v.y - (sgn(dy) * 0.75 + dd * dy/2) * s.acceleration * delta_time * 5
       end
       s.speed = dist(s.v.x, s.v.y) -- update speed
       
