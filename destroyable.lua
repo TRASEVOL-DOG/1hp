e8968ebdb9a22ad888eb8a97cd1cc37e65502ee4
Remy <trasevol.dog@gmail.com>
2019-02-22 20:11:31 +0100

    Added white frame on cacti dying and growing back

--- destroyable.lua
+++ destroyable.lua
@@ -12,6 +12,8 @@ function create_destroyable(id, x, y)
     alive               = true,
     killer              = nil,
     t_respawn           = 0,
+    white_frame         = 0,
+    white_skin          = 0,
     skin                = 0 -- 48 ~ 48 + 3 and 54 -- 48 + 4 ~ 48 + 6 and 55
   }
   
@@ -52,25 +54,41 @@ function create_destroyable(id, x, y)
 end
 
 function update_destroyable(s)
-  if not s.alive then 
+  if server_only and not s.alive then
     s.t_respawn = s.t_respawn - delta_time 
     if s.t_respawn < 0 then respawn_destroyable(s) end
+  end
+  
+  if s.white_frame > 0 then
+    s.white_frame = s.white_frame - delta_time
   end
 end
 
 function draw_destroyable(s)
-  all_colors_to(0)
-  spr(s.skin, s.x-1, s.y-2)
-  spr(s.skin, s.x+1, s.y-2)
-  spr(s.skin, s.x, s.y-3)
-  all_colors_to()
-  pal(1,0)
-  spr(s.skin, s.x, s.y-2)
-  pal(1,1)
+  if s.white_frame > 0 then
+    all_colors_to(3)
+    spr(s.white_skin, s.x-1, s.y-2)
+    spr(s.white_skin, s.x+1, s.y-2)
+    spr(s.white_skin, s.x, s.y-3)
+    spr(s.white_skin, s.x, s.y-2)
+    all_colors_to()
+  else
+    all_colors_to(0)
+    spr(s.skin, s.x-1, s.y-2)
+    spr(s.skin, s.x+1, s.y-2)
+    spr(s.skin, s.x, s.y-3)
+    all_colors_to()
+    pal(1,0)
+    spr(s.skin, s.x, s.y-2)
+    pal(1,1)
+  end
 end
 
 function kill_destroyable(s, killer_id)
   if s.alive then
+    s.white_frame = 0.05
+    s.white_skin = s.skin
+  
     s.alive = false
     s.skin = (s.skin < 52) and 54 or 55
     s.t_respawn = 10 + rnd(5)
@@ -92,5 +110,7 @@ function respawn_destroyable(s)
     s.alive = true
     s.skin = 47 + irnd(6)
     s.killer = nil
+    s.white_frame = 0.1
+    s.white_skin = s.skin
   end
 end
